134ff9190b2c14e1930b2a5029f514b6af30ec12
Daniel Steman <danielsteman@daniels-mbp13.home>
2021-04-30 14:40:50 +0200

    changed statisfiles_dirs

--- spotifycluster/settings.py
+++ spotifycluster/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = ['/react-frontend/build/static']
+STATICFILES_DIRS = ['react-frontend/build/static']
 
 import django_heroku
 django_heroku.settings(locals())

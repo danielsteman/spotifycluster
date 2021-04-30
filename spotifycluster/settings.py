4423860bebc50d0337dc2197c0e149e95ffedea7
Daniel Steman <danielsteman@daniels-mbp13.home>
2021-04-30 12:35:19 +0200

    changed wsgi to import whitenoise module

--- spotifycluster/settings.py
+++ spotifycluster/settings.py
@@ -130,10 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import django_heroku
 django_heroku.settings(locals())

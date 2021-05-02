72c6f8180b105d1cc0bdb0853a90079c1d811844
Daniel Steman <danielsteman@daniels-mbp13.home>
2021-05-02 22:26:14 +0200

    added environment variables in the wsgi

--- spotifycluster/wsgi.py
+++ spotifycluster/wsgi.py
@@ -14,6 +14,8 @@ import os
 # from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotifycluster.settings')
+os.environ['DJANGO_CONFIGURATION'] = 'production'
+os.environ['PRODUCTION_BASE_URL'] = 'https://nameless-taiga-02413.herokuapp.com/'
 
 # application = get_wsgi_application()
 
